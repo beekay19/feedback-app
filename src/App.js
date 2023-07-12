98b5a8b45f00fd5ab794cfbac2c3d87018144555
beekay19 <bknelson19@gmail.com>
2023-07-12 04:01:36 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// eslint-disable-next-line no-unused-vars
+import Header from "./components/header";
 
 function App() {
+  const body = "My App";
+  
+ 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <Header/>
+    <div className="container">
+      <h1>{body.toUpperCase()}</h1>
     </div>
+    </>
   );
 }
 
